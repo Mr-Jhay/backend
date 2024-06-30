def3a09fa9c1022662d64192450f01ee58395371
Mr-Jhay <gumabonjayar6@gmail.com>
2024-06-30 23:30:24 +0800

    first commit

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register',[AuthController::class,'register']);
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::group([
+    "middleware" => "auth:sanctum"
+], function(){
+    Route::get('userprofile',[AuthController::class,'userprofile']);
+    Route::get('logout',[AuthController::class,'logout']);
+});
